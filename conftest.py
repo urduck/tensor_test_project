5add969a46f830009806561f003e6103ffa4b91b
urduck <ramil.zakyat@yandex.ru>
2024-02-07 22:16:46 +0300

    conftest.py

--- conftest.py
+++ conftest.py
@@ -4,6 +4,6 @@ from selenium import webdriver
 
 @pytest.fixture(scope="module")
 def browser():
-    chrome_browser = webdriver.Chrome()
-    chrome_browser.implicitly_wait(20)
-    return chrome_browser
+    driver = webdriver.Firefox()
+    driver.implicitly_wait(10)
+    return driver
